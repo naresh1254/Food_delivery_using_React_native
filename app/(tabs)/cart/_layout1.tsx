dd1a50e50bca22003b7cd60c52cdbb04d8446642
naresh1254 <thedeeve843@gmail.com>
2025-05-15 06:51:31 +0530

    Updated _layout1.tsx

--- app/(tabs)/cart/_layout1.tsx
+++ app/(tabs)/cart/_layout1.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function CartLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index1" options={{ headerShown: false }} />
       <Stack.Screen name="checkout" options={{ headerShown: false }} />
       <Stack.Screen name="success" options={{ headerShown: false }} />
     </Stack>
